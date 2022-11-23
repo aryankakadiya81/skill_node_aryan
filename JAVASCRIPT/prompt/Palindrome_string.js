b8c27475ba9e2b0a4774ff5337ae4ae2aa262b05
ARYAN KAKADIYA <aryankakadiya81@gmail.com>
2022-11-23 22:22:30 +0530

    palindrome string

--- JAVASCRIPT/prompt/Palindrome_string.js
+++ JAVASCRIPT/prompt/Palindrome_string.js
@@ -1,21 +1,37 @@
+// Palindrome string--------------------------------------------
+// let prompt = require("prompt-sync")();
+
+// function Palindro(string) 
+// {
+//     const len = string.length;
+//     for (let i = 0; i < len/2; i++) 
+//     {
+//         if(string[i] != string[len - 1 - i]) 
+//         {
+//             return "string is not a palindrome";
+//         }
+//     }
+//     return "It is a palindrome";
+// }
+
+// const a = prompt('Enter a string: ');
+// console.log(Palindro(a));
+
+
+// Palindrome string //anonymous--------------------------------------
 let prompt = require("prompt-sync")();
 
-
-function palindrome()
-{
-    let str = parseInt(prompt("enter string = "));
-    let len = str.length
-    let revarr = string.reverse();
-    let join = revarr.join("");
-    
-    if(str == join)
+let Palindro=(string)=>{
+    const len = string.length;
+    for (let i = 0; i < len/2; i++) 
     {
-        console.log(`string is palindrome`);
+        if(string[i] === string[len - 1 - i]) 
+        {
+            return "string is a palindrome";
+        }
     }
-    else
-    {
-        console.log(`string is NOT palindrome`);
-    }
+    return "It is not a palindrome";
 }
 
-palindrome();
+const a = prompt('Enter a string: ');
+console.log(Palindro(a));
