464c675692c45a8fee50e34614607f2395254581
ARYAN KAKADIYA <aryankakadiya81@gmail.com>
2022-12-01 09:37:41 +0530

    change_of_callback_task

--- JAVASCRIPT/prompt/total.js
+++ JAVASCRIPT/prompt/total.js
@@ -1,32 +1,95 @@
-// function getSum(n)
+// ----------------------------------------- ARYAN -------------------------------------
+
+// let sum = (number1) => 
 // {
-//     let sum = 0;
-//     while (n != 0) 
+//     let sum1 = 0;
+//     console.log(number1);
+//     while (number1 != 0) 
 //     {
-//         sum = sum + n % 10;
-//         n = parseInt(n / 10);
+//         sum1 = sum1 + number1 % 10;
+//         number1 = parseInt(number1 / 10);
 //     }
-//     return call_back(sum)
-// }
-// function check(val)
-// {
-//     if(val <= 9 && val >= 0)
+//     if (sum1 > 9)
 //     {
-//         return 1;
+//         return sum(sum1);
 //     }
-//     else
-//     {
-//         return getSum(val);
-//     }
-// }
-// function total(n,call_back)
-// {
-//     return callback(n);
+//     return sum1;
 // }
 
-// console.log(total(123456,getSum))
+// let total = (number, callback) => 
+// {
+//     if(number <= 9)
+//     {
+//         return number;
+//     }
+//     return callback(number);
+// }
+// console.log(total(9999999988, sum));
+// console.log(total(1, sum));
+// console.log(total(12, sum));
 
-// -----------------------------------------
+
+// -------------------------------- extr(vrushabh,aryan) -------------------------------
+
+// let sum = (number1) => 
+// {
+
+//     let sum1 = 0;
+//     console.log(number1);
+//     while (number1 != 0) 
+//     {
+//         sum1 = sum1 + number1 % 10;
+//         number1 = parseInt(number1 / 10);
+//     }
+//     return check(sum1);
+
+// }
+
+// let check = (value) => 
+// {
+    
+//     if (value <= 9) {
+//         return value;
+//     }
+//     else 
+//     {
+//        return sum(value);
+//     }
+// }
+
+// let total = (number, callback) => 
+// {
+//     return callback(number);
+// }
+
+// let number = 9898989898;
+// console.log(total(number, sum));
+
+// -------------------------------- vrushabh -------------------------------
+
+// let sum = (number1) => {
+//     let sum1 = 0;
+//     console.log(number1);
+//     while (number1 != 0) {
+//         sum1 = sum1 + number1 % 10;
+//         number1 = parseInt(number1 / 10);
+//     }
+//     return sum1;
+// }
+
+// let total = (number, callback) => {
+//     let n = callback(number);
+//     while (n > 9) {
+//         return sum(n);
+//     }
+//     return n;
+// }
+
+// let number = 9999999988;
+// console.log(total(number, sum));
+
+
+// -------------------------------- paresh sir -------------------------------
 
 // let sum = (number1) => 
 // {
@@ -40,17 +103,6 @@
 //     return sum1;
 // }
 
-// let check = (value) => 
-// {
-//     if (value <= 9) 
-//     {
-//         return value;
-//     }
-//     else 
-//     {
-//        return 0;
-//     }
-// }
 // let total = (number, callback) => 
 // {
 //     let n =  callback(number);
@@ -58,5 +110,6 @@
 //         return sum(n);
 // }
 
-// let number = 123456;
+// let number = 9999999988;
 // console.log(total(number, sum));
+
