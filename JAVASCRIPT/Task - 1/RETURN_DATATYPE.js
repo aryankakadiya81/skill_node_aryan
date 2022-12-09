9cf2ded5c87e9225a816c6d37ac159c8bc205343
ARYAN KAKADIYA <aryankakadiya81@gmail.com>
2022-12-09 18:56:48 +0530

    dom_and_event

---
+++ JAVASCRIPT/Task - 1/RETURN_DATATYPE.js
@@ -0,0 +1,13 @@
+// 9. Write a JS function which accepts an argument and returns the type. 
+
+function dtp(val)
+{
+    return typeof val;
+}
+
+console.log(dtp(10));
+console.log(dtp("aryan"));
+console.log(dtp(true));
+console.log(dtp([]));
+console.log(dtp([1,2,3,4]));
+
