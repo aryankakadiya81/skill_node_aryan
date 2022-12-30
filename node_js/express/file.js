8a24e71daf34e02f66e59b964fc593c763e55a82
ARYAN KAKADIYA <aryankakadiya81@gmail.com>
2022-12-30 18:55:39 +0530

    FINAL CHANGE

---
+++ node_js/express/file.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 2000;
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/log", (req,res) => {
+    res.sendFile(__dirname + "/form.html");
+});
+
+app.post("/log", (req,res) => {
+    const request = req.body;
+    console.log("data",request);
+})
+
+
+// not proper method ----------------------------------- 
+// app.get("/log", (req,res) => {
+//     const request = req.body;
+//     console.log("data get",request);
+// })
+// ----------------------------------------------------- 
+
+app.listen(port, () => {
+    console.log(`port using ${port}`);
+})
