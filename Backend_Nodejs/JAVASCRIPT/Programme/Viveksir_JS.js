51c07d4cf371edd6cd4d70e78982e5bae90cb9c7
aryankakadiya81 <aryankakadiya81@gmail.com>
2023-06-01 09:37:39 +0530

    viveksir_task and componanet added

---
+++ Backend_Nodejs/JAVASCRIPT/Programme/Viveksir_JS.js
@@ -0,0 +1,109 @@
+// 1. Print numbers from 1 to 10
+
+// let i = 1;
+
+// while(i <= 10)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+
+
+
+
+
+// 2. Print the odd numbers less than 100
+
+// let i = 1;
+// while(i <= 100)
+// {
+//     if(i % 2 != 0)
+//     {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+
+
+
+
+// 3. Print the multiplication table with 7
+
+
+// let i = 1;
+// while(i <= 10)
+// {
+//     console.log(`7 * ${i} = ${7*i}`);
+//     i++;
+// }
+
+
+
+
+
+
+// 4.  Print all the multiplication tables with numbers from 1 to 10
+
+
+// let i = 1;
+// while(i <= 10)
+// {
+//     console.log("\n\n")
+//     let j = 1;
+//     while(j <= 10)
+//     {
+//         console.log(`${i} * ${j} = ${i*j}`);
+//         j++;
+//     }
+//     i++;
+// }
+
+
+// 5.Calculate the sum of numbers from 1 to 10
+
+// let n = 10;
+// let sum = 0;
+// for (var i = 1; i <= n; i++) {
+//     sum += i;
+//     // console.log(sum);
+// }
+// console.log(sum);
+
+
+
+
+
+
+
+// 6. Calculate 10! (fectorial)
+
+// let n = 10;
+// let i = 1;
+// let j = 1;
+// while (j <= n) {
+//     i *= j;
+//     j++;
+// }
+// console.log(i);
+
+
+// 7. Calculate the sum of even numbers greater than 10 and less than 30
+
+
+// let i = 11;
+// let sum = 0;
+
+// while(i <= 30)
+// {
+//     if(i % 2 != 0)
+//     {
+//         sum += i;
+//         // console.log(sum);
+//     }
+//     i++;
+// }
+// console.log(sum);
+
